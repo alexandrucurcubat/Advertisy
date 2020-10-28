461a1db11622b7f098e2432fc3804831cf1dacb7
robert-sn <53301329+robert-sn@users.noreply.github.com>
2020-10-28 16:04:27 +0200

    project structure (#1)
    * project structure
    
    * Delete maven-wrapper.properties
    
    * Delete MavenWrapperDownloader.java
    
    * Delete mvnw
    
    * Delete mvnw.cmd
    
    * Update .gitignore

---
+++ src/main/java/ro/student/mtapo/advertisy/AdvertisyApplication.java
@@ -0,0 +1,13 @@
+package ro.student.mtapo.advertisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisyApplication.class, args);
+	}
+
+}
