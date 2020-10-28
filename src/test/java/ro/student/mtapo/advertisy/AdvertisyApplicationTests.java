ca54be5efcc3bcadc5b75499fc0f15d26c973dfd
Balmus Laurentiu <laurentiu.balmus@crystal-system.eu>
2020-10-28 15:55:00 +0200

    project structure

---
+++ src/test/java/ro/student/mtapo/advertisy/AdvertisyApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.student.mtapo.advertisy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
